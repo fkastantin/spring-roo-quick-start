90fa51c0cf8095e64e9d9be705b139145fd4d932
Firas Kastantin <feras.kastanten@gmail.com>
2020-06-13 21:21:00 +0300

    Setup MVC

---
+++ src/main/java/com/app/config/WebMvcConfiguration.java
@@ -0,0 +1,11 @@
+package com.app.config;
+import org.springframework.roo.addon.web.mvc.controller.annotations.config.RooWebMvcConfiguration;
+
+/**
+ * = WebMvcConfiguration
+ TODO Auto-generated class documentation
+ *
+ */
+@RooWebMvcConfiguration
+public class WebMvcConfiguration {
+}
