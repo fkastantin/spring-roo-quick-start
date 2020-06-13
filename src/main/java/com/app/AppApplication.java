dc213cb00c70f92e8a9790df6b15eb97ba859479
Firas Kastantin <feras.kastanten@gmail.com>
2020-06-13 21:13:56 +0300

    Init project

---
+++ src/main/java/com/app/AppApplication.java
@@ -0,0 +1,23 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * = AppApplication
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@SpringBootApplication
+public class AppApplication {
+
+    /**
+     * TODO Auto-generated method documentation
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+}
