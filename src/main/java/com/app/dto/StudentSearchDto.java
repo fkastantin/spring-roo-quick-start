462202fe0e48787b7200ddbec705c189dd57636e
Firas Kastantin <feras.kastanten@gmail.com>
2020-06-13 21:29:54 +0300

    Create StudentSearchDto by firstName and lastName

---
+++ src/main/java/com/app/dto/StudentSearchDto.java
@@ -0,0 +1,25 @@
+package com.app.dto;
+import org.springframework.roo.addon.dto.annotations.RooDTO;
+import org.springframework.roo.addon.javabean.annotations.RooJavaBean;
+
+/**
+ * = StudentSearchDto
+ TODO Auto-generated class documentation
+ *
+ */
+@RooDTO
+@RooJavaBean
+public class StudentSearchDto {
+
+    /**
+     * TODO Auto-generated attribute documentation
+     *
+     */
+    private String firstName;
+
+    /**
+     * TODO Auto-generated attribute documentation
+     *
+     */
+    private String lastName;
+}
